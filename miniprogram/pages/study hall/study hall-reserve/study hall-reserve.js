cdd18d278fbb087f0a3ae51d119ab0952244dd48
Felix <13115412392@163.com>
2019-04-07 15:44:48 +0800

    room

--- miniprogram/pages/study hall/study hall-reserve/study hall-reserve.js
+++ miniprogram/pages/study hall/study hall-reserve/study hall-reserve.js
@@ -5,7 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-    idx:''
+    idx:'',
+    timearray:['半小时','一小时','两小时','三小时'],
+    book:{
+      room_id:'',
+      room_time:'',
+      room_price_yuan:'',
+      room_price:'',
+      user_name:'',
+      user_call:''
+    }
   },
 
   /**
@@ -65,7 +74,64 @@ Page({
   },
   onLoad: function (option) {
     this.setData({
-      idx: option.id
+      idx: option.id,
+      ['book.room_id']:option.id
     });
+  },
+  bindtime: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      timeindex: e.detail.value,
+    })
+    if(e.detail.value==0){
+      this.setData({
+        ['book.room_price_yuan']:0.5*10+'元',
+        ['book.room_price']:0.5*10,
+        ['book.room_time']:0.5
+      })
+    }
+    if (e.detail.value == 1) {
+      this.setData({
+        ['book.room_price_yuan']: 1 * 10 + '元',
+        ['book.room_price']: 1 * 10,
+        ['book.room_time']: 1
+      })
+    }
+    if (e.detail.value == 2) {
+      this.setData({
+        ['book.room_price_yuan']: 2 * 10 + '元',
+        ['book.room_price']: 2 * 10,
+        ['book.room_time']: 2
+      })
+    }
+    if (e.detail.value == 3) {
+      this.setData({
+        ['book.room_price_yuan']: 3 * 10 + '元',
+        ['book.room_price']: 3 * 10,
+        ['book.room_time']: 3
+      })
+    }
+    if (e.detail.value == 4) {
+      this.setData({
+        ['book.room_price_yuan']: 4 * 10 + '元',
+        ['book.room_price']: 4 * 10,
+        ['book.room_time']: 4
+      })
+    }
+  },
+  username (e) {
+    // console.log(e.detail.value)
+    this.setData({
+      ['book.user_name']:e.detail.value
+    })
+  },
+  usercall(e) {
+    // console.log(e.detail.value)
+    this.setData({
+      ['book.user_call']: e.detail.value
+    })
+  },
+  submit () {
+    console.log(this.data.book)
   }
 })
